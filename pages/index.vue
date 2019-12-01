f899b0aae5004f11e9d35ea207a8997b62c577d9
Andresrom1123 <andres2016_romero@hotmail.com>
2019-12-01 13:09:41 -0600

    suebiendo el login e index

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,141 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        clontrello
-      </h1>
-      <h2 class="subtitle">
-        My fantabulous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="-font">
+    <div class="container my-3">
+      <div class="d-flex">
+        <div class="col-6">
+          <h3 class="font-weight-bold">Trabaje con cualquier equipo</h3>
+          <p class="-font-size-1">
+            Ya sea para un trabajo, un proyecto o incluso las próximas
+            vacaciones en familia, Trello ayuda a tu equipo a permanecer
+            organizado.
+          </p>
+          <button type="button" class="btn btn-secondary -font-size-1">
+            Empiece a <i class="ml-2 fas fa-long-arrow-alt-right"></i>
+          </button>
+          <div></div>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div class="col-6"></div>
+        <div>
+          <h3 class="font-weight-bold">
+            Información de un vistazo
+          </h3>
+          <p class="-font-size-1">
+            Profundice en los detalles añadiendo comentarios, archivos adjuntos,
+            fechas de vencimiento y mucho más directamente en las tarjetas de
+            Trello. Colabore en los proyectos de principio a fin.
+          </p>
+        </div>
+      </div>
+      <!--  -->
+      <div>
+        <h3 class="font-weight-bold text-center">Trello a su manera</h3>
+        <div class="d-flex justify-content-center">
+          <p class="-font-size-1 text-center col-6">
+            Use Trello del modo que crea oportuno para aprovechar al máximo las
+            virtudes de su equipo. Somos lo bastante flexibles y ofrecemos
+            funcionalidades de sobra para ajustarnos al estilo y las necesidades
+            de cada equipo.
+          </p>
+        </div>
+        <!--  -->
+        <div class="row">
+          <div class="col-4">
+            <div class="text-center">
+              <img
+                src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/0cad30a99820b0d840a5b48635d00b6e/updated-layouts-collab.png"
+                alt=""
+                class="-img-2"
+              />
+            </div>
+            <h4 class="text-center font-weight-bold">
+              Manual de estrategia del equipo
+            </h4>
+            <p class="text-center -text-p">
+              No encontrará complicaciones a la hora de configurar y poner a
+              todo su equipo a trabajar con Trello. Hemos reunido todos los
+              tableros y herramientas que necesita para lograr el éxito en un
+              práctico recurso.
+            </p>
+            <div class="d-flex justify-content-center">
+              <button type="button" class="btn btn-secondary">
+                Trace un plan de juego
+              </button>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <img
+                src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/15e77c8347135120a438d17e38195e44/updated-layouts-platform.png"
+                alt=""
+                class="-img-2"
+              />
+            </div>
+            <h4 class="text-center font-weight-bold">
+              Una plataforma de productividad
+            </h4>
+            <p class="text-center -text-p">
+              Integre directamente en su flujo de trabajo las aplicaciones que
+              su equipo ya utiliza. Los Power-Up convierten los tableros de
+              Trello en aplicaciones vivas para cumplir con las necesidades
+              particulares de su equipo.
+            </p>
+            <div class="d-flex justify-content-center">
+              <button type="button" class="btn btn-secondary">
+                Impulse su flujo de trabajo
+              </button>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <img
+                src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/6eaaf5e37ab67a8ed6cd7764660513b9/updated-layouts-sync.png"
+                alt=""
+                class="-img-2"
+              />
+            </div>
+            <h4 class="text-center font-weight-bold">
+              Siempre sincronizado
+            </h4>
+            <p class="text-center -text-p">
+              Trello permanece sincronizado en todos sus dispositivos allá donde
+              esté. Colabore con su equipo desde cualquier lugar, ya sea en el
+              autobús o en la playa.
+            </p>
+            <div class="text-center">
+              <img
+                src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/eebf313cb223112b503b7322173b013c/btn-appstore-black.png"
+                alt=""
+                class="-img -pointer"
+              />
+              <img
+                src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/bcde9868a665af4ab4efe460c46fbc7b/btn-playstore-black.png"
+                alt=""
+                class="-img -pointer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="-bg align-items-center d-flex justify-content-center">
+      <div>
+        <h3 class="font-weight-bold text-center">
+          Póngase a planificar ya mismo
+        </h3>
+        <p class="text-center -font-size-1">
+          Regístrese y pase a ser una de los millones de personas de <br />
+          personas de todo el mundo que utilizan Trello <br />
+          para hacer más y mejor las cosas.
+        </p>
+        <div class="d-flex justify-content-center">
+          <button type="button" class="btn btn-success -font-size-1">
+            Empiece ya. Es gratis!
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ pages/login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="-banner -font">
+    <div class="d-flex justify-content-center pt-5 pb-2">
+      <img
+        src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/76ceb1faa939ede03abacb6efacdde16/trello-logo-blue.svg"
+        alt=""
+        class="-img"
+      />
+    </div>
+    <div class="d-flex justify-content-center">
+      <div class="shadow p-5 -form">
+        <div class="d-flex justify-content-center mb-3 font-weight-bold">
+          <template>Iniciar sesion con trello</template>
+        </div>
+        <form>
+          <div class="form-group">
+            <input
+              id="exampleInputEmail1"
+              type="email"
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Introduzca el correo electronico"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              id="exampleInputPassword1"
+              type="password"
+              class="form-control"
+              placeholder="Introduzca la contraseña"
+            />
+          </div>
+          <button type="submit" class="btn btn-block btn-success">
+            Iniciar sesion
+          </button>
+        </form>
+        <div class="border-bottom my-4"></div>
+        <ul class="text-center">
+          <li class="-list-style-none mb-2">
+            <a href="" class="text-decoration-none -text-blue font-weight-bold">
+              No puedes iniciar sesion?
+            </a>
+          </li>
+          <li class="-list-style-none">
+            <a href="" class="text-decoration-none -text-blue font-weight-bold">
+              Registrese para una cuenta nueva
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
