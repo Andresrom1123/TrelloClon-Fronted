60323370fcb38db76a7a174db2c291b6d5c14646
Derian-Web <id1474416@outlook.com>
2019-12-02 10:49:03 -0600

    modificacion index

---
+++ layouts/index-layaout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light">
+      <img src="/trello-logo.svg" width="200" class="pr-5" />
+      <form class="form-inline">
+        <h6 class="pr-3 .bg-white">Iniciar seccion</h6>
+        <button
+          class="btn btn-outline-success my-2 my-sm-0"
+          type="submit"
+          id="fondo"
+        >
+          Registrate
+        </button>
+      </form>
+    </nav>
+    <nuxt />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  background: linear-gradient(135deg, #0079bf, #5067c5);
+}
+h6 {
+  color: #ffffff;
+}
+#fondo {
+  background: #ffffff;
+  color: #0178bf;
+  border: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,72 @@
 <template>
   <div class="-font">
+    <div class="background pt-4">
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col">
+            <p class="size1">
+              Trello le permite trabajar de forma más colaborativa y ser más
+              productivo.
+            </p>
+            <p class="text">
+              Las tarjetas, listas y tableros de Trello le permiten organizar y
+              priorizar sus proyectos de forma divertida, flexible y provechosa.
+            </p>
+          </div>
+          <div class="col">
+            <img src="/img.svg" alt="" />
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-4">
+            <input
+              type="text"
+              class="correo"
+              placeholder="Correo electrónico"
+            />
+          </div>
+          <div class="col-4">
+            <button
+              type="button"
+              class="btn btn-wrap btn-success btn-lg px-4"
+              id="register"
+            >
+              Registrese Gratis
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Terminacion del Baner -->
     <div class="container my-3">
-      <div class="d-flex">
-        <div class="col-6">
+      <!--  -->
+      <div class="row d-flex align-items-center">
+        <div class="col">
           <h3 class="font-weight-bold">Trabaje con cualquier equipo</h3>
-          <p class="-font-size-1">
+          <p class="sub-text">
             Ya sea para un trabajo, un proyecto o incluso las próximas
             vacaciones en familia, Trello ayuda a tu equipo a permanecer
             organizado.
           </p>
-          <button type="button" class="btn btn-secondary -font-size-1">
-            Empiece a <i class="ml-2 fas fa-long-arrow-alt-right"></i>
-          </button>
-          <div></div>
+        </div>
+        <div class="col">
+          <img class="rounded" src="/tabla.png" width="500" height="320" />
         </div>
       </div>
-      <div class="d-flex">
-        <div class="col-6"></div>
-        <div>
-          <h3 class="font-weight-bold">
-            Información de un vistazo
-          </h3>
-          <p class="-font-size-1">
+      <div class="row pt-3 d-flex align-items-center">
+        <div class="col">
+          <img class="rounded" src="/tel.png" width="400" height="259" />
+        </div>
+        <div class="col">
+          <h3 class="font-weight-bold">Información de un vistazo</h3>
+          <p class="sub-text">
             Profundice en los detalles añadiendo comentarios, archivos adjuntos,
             fechas de vencimiento y mucho más directamente en las tarjetas de
             Trello. Colabore en los proyectos de principio a fin.
           </p>
         </div>
       </div>
-      <!--  -->
-      <div>
+      <div class="pt-4">
         <h3 class="font-weight-bold text-center">Trello a su manera</h3>
         <div class="d-flex justify-content-center">
           <p class="-font-size-1 text-center col-6">
@@ -118,6 +155,43 @@
           </div>
         </div>
       </div>
+      <div class="container pt-4">
+                <div class="container pt-4" id="sub-background">
+                  <div class="row">
+                    <div class="col">
+                      <p class="sub-title">
+                        Trabaje de manera más inteligente con Trello
+                      </p>
+                      <p class="text">
+                        Empresas de todas las índoles y tamaños utilizan Trello.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-3 pr-4 pt-3">
+                      <img src="/logo-ki.svg" alt="" />
+                    </div>
+                    <div class="col-3 pl-3 pt-3">
+                      <img src="/logo-national.svg" alt="" />
+                    </div>
+                    <div class="col-3 pl-3 pt-3">
+                      <img src="/logo-google.svg" alt="" />
+                    </div>
+                    <div class="col-3 pl-3 pt-3">
+                      <img src="/logo-fender.svg" alt="" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <button
+                      type="button"
+                      class="btn btn-light p-2 pt-2"
+                      id="buttn"
+                    >
+                      Descubrir como
+                    </button>
+                  </div>
+                </div>
+              </div>
     </div>
     <!--  -->
     <div class="-bg align-items-center d-flex justify-content-center">
@@ -125,11 +199,6 @@
         <h3 class="font-weight-bold text-center">
           Póngase a planificar ya mismo
         </h3>
-        <p class="text-center -font-size-1">
-          Regístrese y pase a ser una de los millones de personas de <br />
-          personas de todo el mundo que utilizan Trello <br />
-          para hacer más y mejor las cosas.
-        </p>
         <div class="d-flex justify-content-center">
           <button type="button" class="btn btn-success -font-size-1">
             Empiece ya. Es gratis!
@@ -139,3 +208,69 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  layout: 'index-layaout'
+}
+</script>
+<style scoped>
+* {
+  font-family: 'Open Sans', sans-serif;
+}
+.size1 {
+  font-size: 48px;
+  line-height: 58px;
+  color: #ffffff;
+  font-weight: 500;
+}
+.background {
+  background: linear-gradient(135deg, #0079bf, #5067c5);
+  min-height: 100vh;
+}
+.text {
+  color: #ffffff;
+  font-size: 27px;
+  font-weight: 300;
+}
+.correo {
+  flex-grow: 5;
+  min-width: 0;
+  width: 376px;
+  border-radius: 0.3rem;
+  line-height: 43px;
+  padding: 4px;
+  padding-left: 16px;
+  border-color: white;
+  border-style: solid;
+}
+#register {
+  height: 55px;
+  width: 300px;
+}
+#sub-background {
+  background: linear-gradient(180deg, #42548e 0%, #6b668c 100%);
+}
+.sub-title {
+  font-size: 35px;
+  color: #ffffff;
+  text-align: center;
+}
+.text {
+  font-size: 24px;
+  color: #ffffff;
+  text-align: center;
+}
+#buttn {
+  margin: auto;
+  font-size: 20px;
+}
+h3 {
+  color: rgb(23, 43, 77);
+  font-size: 35px;
+  font-weight: 500;
+}
+.sub-text {
+  color: rgb(23, 43, 77);
+  font-size: 20px;
+}
+</style>
